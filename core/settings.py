c05a83ee2f079c310c3b4cf4284b6933f7a40ae1
AnhDH <anhdhbn@gmail.com>
2020-10-20 01:45:34 +0700

    init

--- core/settings.py
+++ core/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = config('SECRET_KEY', default='S#perS3crEt_1122')
 DEBUG = config('DEBUG', default=False)
 
 # load production server from .env
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', config('SERVER', default='127.0.0.1'), '7325e2e9f003.ngrok.io']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', config('SERVER', default='127.0.0.1'), '7325e2e9f003.ngrok.io', 'aqm-u.herokuapp.com']
 
 # Application definition
 
