d52e965dcb342564d6e36ba34167732fcc7f00c9
AnhDH <anhdhbn@gmail.com>
2020-12-26 08:58:13 -0800

    fix >20

--- gunicorn-cfg.py
+++ gunicorn-cfg.py
@@ -4,8 +4,8 @@ Copyright (c) 2019 - present AppSeed.us
 """
 
 bind = '0.0.0.0:5005'
-workers = 5
+workers = 1
 accesslog = '-'
-loglevel = 'error'
+loglevel = 'info'
 capture_output = True
 enable_stdio_inheritance = True
